8b7846f539354077d4558aca4ebef48d89c64355
Aster Valdemoro <astervaldemoro@gmail.com>
2018-06-20 18:47:45 -0400

    refactor: parsed JSON to sandwich object and other files added so this thing can build properly

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,49 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.*;
+import java.util.*;
+
 public class JsonUtils {
+    public static final String  NAME = "name",
+                                MAIN_NAME = "mainName",
+                                ALSO_KNOWN_AS = "alsoKnownAs",
+                                PLACE_OF_ORIGIN = "placeOfOrigin",
+                                DESCRIPTION = "description",
+                                IMAGE = "image",
+                                INGREDIENTS = "ingredients";
+
+
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject root = new JSONObject(json);
+
+            //get root object
+            JSONObject name = root.getJSONObject(NAME);
+
+            //set object values
+            sandwich.setMainName(name.getString(MAIN_NAME));
+            sandwich.setAlsoKnownAs(getJsonArrayAsList(name.getJSONArray(ALSO_KNOWN_AS)));
+            sandwich.setPlaceOfOrigin(root.getString(PLACE_OF_ORIGIN));
+            sandwich.setDescription(root.getString(DESCRIPTION));
+            sandwich.setImage(root.getString(IMAGE));
+            sandwich.setIngredients(getJsonArrayAsList(root.getJSONArray(INGREDIENTS)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
+    }
+    private static List<String> getJsonArrayAsList(JSONArray jsonArray) {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            for(int i = 0; i < jsonArray.length(); i++) {
+                list.add(jsonArray.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
